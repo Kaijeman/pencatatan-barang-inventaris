f6be6ed4b8d4ba526756380200a9765a1727208e
Kaijeman <fachrudy.khalief@gmail.com>
2026-07-15 10:02:51 +0800

    feat:add SoftDeletes to category, supplier, and item

--- database/migrations/2026_07_15_005932_create_items_table.php
+++ database/migrations/2026_07_15_005932_create_items_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->integer('minimum_stock')->default(0);
             $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
